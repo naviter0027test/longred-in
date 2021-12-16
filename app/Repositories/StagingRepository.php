41ed3a66cbe9d78c03bc2c64bbef7e322b42616e
cp5 <cp5@test0068.axcell28.idv.tw>
2021-12-16 12:00:53 +0800

    使用者 api:分期表上傳圖檔完成

--- app/Repositories/StagingRepository.php
+++ app/Repositories/StagingRepository.php
@@ -10,16 +10,22 @@ use Storage;
 class StagingRepository
 {
     public function create($params, $files = []) {
-        /*
+        $staging = new Staging();
+        $staging->title = '';
+        $staging->img = '';
+        $staging->area = isset($params['area']) ? $params['area'] : 1;
+        $staging->save();
         $root = config('filesystems')['disks']['uploads']['root'];
         $path = date('/Y/m'). '/';
         if(isset($files['img'])) {
+            $originName = $files['img']->getClientOriginalName();
+            $originName = explode('.', $originName)[0];
             $ext = $files['img']->getClientOriginalExtension();
-            $filename = $staging->id. "_pic1.$ext";
+            $filename = $staging->id. "_img.$ext";
+            $staging->title = $originName;
             $staging->img = $path. $filename;
             $staging->save();
             $files['img']->move($root. $path, $filename);
         }
-         */
     }
 }
