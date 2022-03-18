00521c7bad646cce1e5599e0e5fc6526174b8a78
cp5 <cp5@test0068.axcell28.idv.tw>
2022-03-18 15:41:32 +0800

    vue: 取得目前已設定查看權限

--- public/js/vue/home.js
+++ public/js/vue/home.js
@@ -1,6 +1,7 @@
 var v = new Vue({
     el: '#vueApp',
     data: {
+        selectize: null,
         privileges: [],
         privilegesSelected: []
     },
@@ -18,33 +19,7 @@ var v = new Vue({
                 }
             }
         });
-        $.ajax({
-            'type': "GET",
-            'url': "/user/privileges",
-            'success': function(data) {
-                console.log(data);
-                var json = JSON.parse(data);
-                if(json['status'] == true) {
-                    selfthis.privileges = json['privileges'];
-                    setTimeout(function() {
-                        $('.pri-select select').selectize({
-                            'create': false,
-                            'onChange': function(value) {
-                                //console.log(value);
-                                if(value == null)
-                                    selfthis.privilegesSelected = [];
-                                else
-                                    selfthis.privilegesSelected = value;
-                            },
-                            'sortField': {
-                                'field': 'text',
-                                'direction': 'asc'
-                            }
-                        });
-                    }, 1000);
-                }
-            }
-        });
+        this.getPrivileges();
     },
     watch: {
         privilegesSelected: function() {
@@ -53,6 +28,50 @@ var v = new Vue({
         }
     },
     methods: {
+        getPrivileges: function() {
+            console.log('getPrivileges');
+            var selfthis = this;
+            $.ajax({
+                'type': "GET",
+                'url': "/user/privileges",
+                'success': function(data) {
+                    console.log(data);
+                    var json = JSON.parse(data);
+                    if(json['status'] == true) {
+                        selfthis.privileges = json['privileges'];
+                        setTimeout(function() {
+                            var $select = $('.pri-select select').selectize({
+                                'create': false,
+                                'onChange': function(value) {
+                                    //console.log(value);
+                                    if(value == null)
+                                        selfthis.privilegesSelected = [];
+                                    else
+                                        selfthis.privilegesSelected = value;
+                                },
+                                'sortField': {
+                                    'field': 'text',
+                                    'direction': 'asc'
+                                }
+                            });
+                            selfthis.selectize = $select[0].selectize;
+                            selfthis.getSeePrivileges();
+                        }, 1000);
+                    }
+                }
+            });
+        },
+        getSeePrivileges: function() {
+            var selfthis = this;
+            $.get('/user/privileges/getsee', function(resData) {
+                console.log(resData);
+                var resJson = JSON.parse(resData);
+                if(resJson['status'] == true) {
+                    selfthis.privilegesSelected = resJson['privileges'];
+                    selfthis.selectize.setValue(selfthis.privilegesSelected);
+                }
+            });
+        },
         logout: function() {
             if(confirm('確定登出?')) {
                 var href = $(this).attr('href');
