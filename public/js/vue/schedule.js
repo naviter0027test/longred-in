1f778c61f0fa846700502841b0cd2fe86ab075c9
cp5 <cp5@test0068.axcell28.idv.tw>
2022-03-22 10:32:31 +0800

    vue: 歸檔進度(/user/case/schedule)列表的介接與顯示

--- public/js/vue/schedule.js
+++ public/js/vue/schedule.js
@@ -1,14 +1,39 @@
 var v = new Vue({
     el: '#vueApp',
     data: {
-        'keyword': ''
+        'records': [],
+        'keyword': '',
+        'CustProjectStatus': '核准無缺',
+        'nowPage': 1,
+        'offset': 9999
     },
     created: function() {
         console.log('created');
+        var selfthis = this;
+        var postData = {};
+        postData['keyword'] = this.keyword;
+        postData['CustProjectStatus'] = this.CustProjectStatus;
+        postData['nowPage'] = this.nowPage;
+        postData['offset'] = this.offset;
+        $.ajax({
+            'type': "POST",
+            'url': "/user/case/schedule",
+            'data': postData,
+            'success': function(data) {
+                var json = JSON.parse(data);
+                if(json['result'] == true) {
+                    selfthis.records = json.records;
+                    console.log(json);
+                }
+            }
+        });
     },
     methods: {
         search: function() {
             console.log('search');
+        },
+        see: function(index) {
+            console.log('see'+ index);
         },
         logout: function() {
             if(confirm('確定登出?')) {
