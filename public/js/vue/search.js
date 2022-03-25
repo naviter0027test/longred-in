cfae09374b3b1ac1306e0cdc673923c2f17a8d63
cp5 <cp5@test0068.axcell28.idv.tw>
2022-03-25 10:17:58 +0800

    vue: 案件查詢 狀態切換的介接處理完成

--- public/js/vue/search.js
+++ public/js/vue/search.js
@@ -4,6 +4,11 @@ var v = new Vue({
         'records': [],
         'keyword': '',
         'CustProjectStatus': '',
+        'status1': true,
+        'status2': false,
+        'status3': false,
+        'status4': false,
+        'status5': false,
         'nowPage': 1,
         'offset': 9999
     },
@@ -21,11 +26,54 @@ var v = new Vue({
                 }
             }
         });
-        this.search();
+        var custProjectStatus = this.getUrlParameter('CustProjectStatus');
+        this.statusChoice(custProjectStatus);
     },
     methods: {
+        statusChoice: function(custProjectStatus) {
+            console.log(custProjectStatus);
+            if(custProjectStatus == false) {
+                custProjectStatus = '';
+                this.status1 = true;
+                this.status2 = false;
+                this.status3 = false;
+                this.status4 = false;
+                this.status5 = false;
+            }
+            else if(custProjectStatus == '處理中') {
+                this.status1 = false;
+                this.status2 = true;
+                this.status3 = false;
+                this.status4 = false;
+                this.status5 = false;
+            }
+            else if(custProjectStatus == '待核准') {
+                this.status1 = false;
+                this.status2 = false;
+                this.status3 = true;
+                this.status4 = false;
+                this.status5 = false;
+            }
+            else if(custProjectStatus == '核准') {
+                this.status1 = false;
+                this.status2 = false;
+                this.status3 = false;
+                this.status4 = true;
+                this.status5 = false;
+            }
+            else if(custProjectStatus == '婉拒取消') {
+                this.status1 = false;
+                this.status2 = false;
+                this.status3 = false;
+                this.status4 = false;
+                this.status5 = true;
+            }
+            this.CustProjectStatus = custProjectStatus;
+            this.search();
+        },
         search: function() {
-            console.log('search:'+ this.keyword);
+            console.log('search key:'+ this.keyword);
+            console.log('search status:'+ this.CustProjectStatus);
             var selfthis = this;
             var postData = {};
             postData['keyword'] = this.keyword;
@@ -50,6 +98,21 @@ var v = new Vue({
             console.log(this.records[index]);
             window.sessionStorage.setItem('search-record', JSON.stringify(this.records[index]));
             //location.href = "/vue/search-see.html";
+        },
+        getUrlParameter: function(sParam) {
+            var sPageURL = window.location.search.substring(1),
+                sURLVariables = sPageURL.split('&'),
+                sParameterName,
+                i;
+
+            for (i = 0; i < sURLVariables.length; i++) {
+                sParameterName = sURLVariables[i].split('=');
+
+                if (sParameterName[0] === sParam) {
+                    return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+                }
+            }
+            return false;
         },
         logout: function() {
             if(confirm('確定登出?')) {
