eb3440eca6c9a1c4ef934affa7341b680ea0d1ce
cp5 <cp5@test0068.axcell28.idv.tw>
2022-04-01 17:51:51 +0800

    vue: 客戶繳款資訊查詢、明細。廠商帳密查詢、明細

---
+++ public/js/vue/company.js
@@ -0,0 +1,82 @@
+var v = new Vue({
+    el: '#vueApp',
+    data: {
+        'records': [],
+        'keyword': '',
+        'nowPage': 1,
+        'offset': 9999
+    },
+    created: function() {
+        console.log('created');
+        $.ajax({
+            'type': "GET",
+            'url': "/user/isLogin",
+            'success': function(data) {
+                console.log(data);
+                var json = JSON.parse(data);
+                if(json['status'] == false) {
+                    alert("尚未登入");
+                    location.href = "/vue/login.html";
+                }
+            }
+        });
+        this.search();
+    },
+    methods: {
+        search: function() {
+            console.log('search key:'+ this.keyword);
+            var selfthis = this;
+            var postData = {};
+            postData['keyword'] = this.keyword;
+            postData['nowPage'] = this.nowPage;
+            postData['offset'] = this.offset;
+            $.ajax({
+                'type': "POST",
+                'url': "/user/companies",
+                'data': postData,
+                'success': function(data) {
+                    var json = JSON.parse(data);
+                    if(json['result'] == true) {
+                        selfthis.records = json.companies;
+                        console.log(json);
+                    }
+                }
+            });
+        },
+        see: function(index) {
+            console.log('see:'+ index);
+            console.log(this.records[index]);
+            window.sessionStorage.setItem('company-record', JSON.stringify(this.records[index]));
+            location.href = "/vue/company-see.html";
+        },
+        getUrlParameter: function(sParam) {
+            var sPageURL = window.location.search.substring(1),
+                sURLVariables = sPageURL.split('&'),
+                sParameterName,
+                i;
+
+            for (i = 0; i < sURLVariables.length; i++) {
+                sParameterName = sURLVariables[i].split('=');
+
+                if (sParameterName[0] === sParam) {
+                    return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+                }
+            }
+            return false;
+        },
+        logout: function() {
+            if(confirm('確定登出?')) {
+                var href = $(this).attr('href');
+                $.get('/user/logout', function(resData) {
+                    console.log(resData);
+                    var resJson = JSON.parse(resData);
+                    if(resJson['status'] == true) {
+                        alert('登出成功');
+                        location.href = "/vue/login.html";
+                    }
+                });
+            }
+            return false;
+        }
+    }
+});
